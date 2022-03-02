45d9e2da25a8ee959d07f023ebc1947b8157e0f3
Tim <t.jones@timmoth.com>
2022-03-02 08:23:42 +0000

    Added Component Component Collision Tests

---
+++ Tests/Component/Collision/CollisionTests.cs
@@ -0,0 +1,19 @@
+﻿using Aptacode.AppFramework.Components;
+using Xunit;
+
+namespace Tests.Component.Collision;
+
+public class CollisionTests
+{
+    [Theory]
+    [ClassData(typeof(ComponentComponentCollisionTestDataGenerator))]
+    public void ComponentComponentCollision(ComponentViewModel a, ComponentViewModel b, bool shouldCollide)
+    {
+        //Arrange
+        //Act
+        var sut = a.CollidesWith(b);
+
+        //Assert
+        Assert.Equal(shouldCollide, sut);
+    }
+}
---
+++ Tests/Component/Collision/ComponentComponentCollisionTestDataGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Aptacode.AppFramework.Components.Primitives;
+using Aptacode.Geometry.Primitives;
+
+namespace Tests.Component.Collision;
+
+public class ComponentComponentCollisionTestDataGenerator : IEnumerable<object[]>
+{
+    private readonly List<object[]> _data = new()
+    {
+        new object[] { Point.Zero.ToViewModel(), Point.Zero.ToViewModel(), true },
+        new object[] { Point.Unit.ToViewModel(), Point.Zero.ToViewModel(), false },
+    };
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        return _data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
